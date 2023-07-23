803e32e6c3a4bbfd3ebe5148ae45ac2236804d98
karandeeppotato <karan.sehgal.301202@gmail.com>
2023-07-24 02:06:19 +0530

    Header Component

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png';
+
+const header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="SmartCart" height="45x" width="45px"></img>{' '}
+            SmartCart
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <FaShoppingCart /> cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <FaUser /> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default header;
