71c60d0b4db288f4f36fee3cdfbb726568ca9759
karandeeppotato <karan.sehgal.301202@gmail.com>
2023-07-24 02:07:04 +0530

    App Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/header';
+import Footer from './components/footer';
 
 const App = () => {
-  return <h1>Welcome to SmartCart</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to SmartCart</h1>;
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
