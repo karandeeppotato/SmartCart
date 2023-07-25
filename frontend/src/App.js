b881f4e8dffe08105200feefb6c9fd50d8024b8c
karandeeppotato <karan.sehgal.301202@gmail.com>
2023-07-26 02:08:53 +0530

    Routing and product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 import Header from './components/header';
 import Footer from './components/footer';
 
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to SmartCart</h1>;
+          <Outlet />
         </Container>
       </main>
       <Footer />
